d30b3bd5d5fd6162a3f6c77d37bfb238ea7e08b2
Ronaldo Rodriguez <vixis2014@gmail.com>
2023-11-14 13:13:55 -0600

    i hope this doesnt do anything i dont want it to d

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+#SETUP app to use a sqlachmey database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sampledb.db'
+
+db = SQLAlchemy(app)
+
+class Visitor(db.Model):
+    username = db.Column(db.String(100), primary_key=True)
+    numVisits = db.Column(db.Integer, default=1)
+
+    def __repr__(self):
+        return f"{self.username} - {self.numVisits}"
+    
+#create tables in Database
+with app.app_context():
+    db.create_all()
+
+#function to read in details for page
 # Make a homepage   
 @app.route('/')
 def homepage():
@@ -11,6 +29,10 @@ def homepage():
 def hello(name):
     listOfNames = [name, "Chums", "Chester"]
     return render_template('name.html', name=name, nameList=listOfNames)
+
+@app.route('/visitor>')
+def wat(name):
+    return render_template('visitors.html')
 
 @app.route('/form', methods=['GET', 'POST'])
 def formDemo(name=None):
